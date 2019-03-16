832675a9e691a478a831bf573c4d5d9efebfd092
Harris Elliman <harris.elliman@hotmail.co.uk>
2019-03-16 19:54:01 +0000

    add initial edit route and page

--- app.js
+++ app.js
@@ -76,7 +76,11 @@ app.get("/blogs/:id", function(req, res){
     });
 });
 
+// EDIT ROUTE
 
+app.get("/blogs/:id/edit", function(req, res){
+    res.render("edit");
+});
 
 
 app.listen(process.env.PORT, process.env.IP, function(){
