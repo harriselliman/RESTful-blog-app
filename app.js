43757759f961a068fad4bfafdad34137eb837215
Harris Elliman <harris.elliman@hotmail.co.uk>
2019-03-16 16:30:27 +0000

    fix app setup to include express

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var express     = require("express"),
     
 mongoose.connect("mongodb://localhost:27017/restful_blog_app", { useNewUrlParser: true});
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 // MONGOOSE/MODEL CONFIG
