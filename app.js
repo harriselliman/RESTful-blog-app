71e1117a6a94c47a25a2dea91138fb8cd2d89e11
Harris Elliman <harris.elliman@hotmail.co.uk>
2019-03-16 15:14:20 +0000

    add initial new blog route

--- app.js
+++ app.js
@@ -32,6 +32,8 @@ app.get("/", function(req, res){
     res.redirect("/blogs");
 });
 
+// INDEX ROUTE
+
 app.get("/blogs", function(req, res){
     Blog.find({}, function(err, blogs){
         if(err) {
@@ -41,6 +43,16 @@ app.get("/blogs", function(req, res){
         }
     });
 });
+
+// NEW ROUTE
+
+app.get("/blogs/new", function(req, res){
+    res.render("new");
+})
+
+
+// CREATE ROUTE
+
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server is running");
